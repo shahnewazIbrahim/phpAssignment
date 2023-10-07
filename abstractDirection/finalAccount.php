1b5e5cc75255309ae5b3e2299e6b003cc791eb1c
Mamba-dot <shahnewaz886@gmail.com>
2023-10-07 21:34:26 +0600

    final

--- abstractDirection/finalAccount.php
+++ abstractDirection/finalAccount.php
@@ -6,9 +6,17 @@ require'./Practice/Project1/public/autoload.php';
 require'Profit.php';
 
 class FinalAccount extends Profit {
+    private $quantity = 0;
+    private float $price = 0.00;
+
+    public function __construct($quantity = 0, $price = 0.00) 
+    {
+        $this->quantity = $quantity;
+        $this->price = $price;
+    }
     public function totalRevenue():float{}
     public function totalSale():int{}
     public function averagePrice():float{}
 }
 
-new FinalAccount();
+$finalAccount = new FinalAccount();
