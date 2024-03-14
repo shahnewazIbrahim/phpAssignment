252635373acd5a7dd73cd07d46e17cfc504191c4
shahnewazIbrahim <shahnewaz886@gmail.com>
2024-03-14 17:16:02 +0600

    final project

--- Final Project/Jobpulse/resources/views/layout/app.blade.php
+++ Final Project/Jobpulse/resources/views/layout/app.blade.php
@@ -24,7 +24,7 @@
         </div>
     </div>
 
-    <header class="bg-gradient-primary py-3">
+    <header class="bg-gradient-dark py-3">
         <div class="d-flex justify-content-between align-items-center container">
             <a href="{{ url('/') }}"
                 class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
@@ -72,7 +72,7 @@
         @yield('content')
     </div>
 
-    <footer class="footer py-4 bg-primary">
+    <footer class="footer py-4 bg-gradient-dark">
         <div class="container">
             <div class="row">
                 <div class="col-lg-4 mb-4 mb-lg-0">
